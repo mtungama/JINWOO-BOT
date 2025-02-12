d1d47b059d5576fb28c89528c6b1439da9049b00
mtungama <criscomtungama@gmail.com>
2025-02-13 01:00:45 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -74,7 +74,7 @@ global.timezones = process.env.TIMEZONE || "Africa/Nairobi"
 //Don't edit this if you don't know!
 
 //=================================================//
-global.autoread = process.env.AUTO_READ || 'false';
+global.autoread = process.env.AUTO_READ || 'true';
 // Set to 'true' to enable automatic reading of messages
 
 //=================================================//
